9e250ebf9355c5a98f39908e72f3f3f1f8b5b1e1
th3-j4ck4l <maha678@icloud.com>
2019-10-14 15:08:44 +0300

    yine patter hatasi mk

--- app.py
+++ app.py
@@ -7,11 +7,8 @@ app = Flask(__name__)
 
 
 @app.route("/api/check-team/<string:region>/", methods=["GET"], strict_slashes=False)
-def check_team(region):
-    return jsonify(worker.check_server(region))
-
 @app.route("/api/check-team/<string:region>/<int:port>", methods=["GET"], strict_slashes=False)
-def check_team_by_port(region, port):
+def check_team(region, port=None):
     return jsonify(worker.check_server(region, port=port))
 
 @app.route("/api/check-teams", methods=["GET"], strict_slashes=False)
