c203530eb2fc8f3623e816340697ccd85b2f1fc4
th3-j4ck4l <maha678@icloud.com>
2019-10-14 14:59:03 +0300

    fix slash strict

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import worker
 app = Flask(__name__)
 
 
-@app.route("/api/check-team/<string:region>/", methods=["GET"])
+@app.route("/api/check-team/<string:region>/", methods=["GET"], strict_slashes=False)
 def check_team(region):
     return jsonify(worker.check_server(region))
 
-@app.route("/api/check-team-port/<string:region>/<int:port>", methods=["GET"])
+@app.route("/api/check-team-port/<string:region>/<int:port>", methods=["GET"], strict_slashes=False)
 def check_team_by_port(region, port):
     return jsonify(worker.check_server_by_port(region, port))
 
-@app.route("/api/check-teams", methods=["GET"])
+@app.route("/api/check-teams", methods=["GET"], strict_slashes=False)
 def checkTeams():
     return jsonify(worker.check_servers())
 
