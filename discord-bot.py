fa6e0f07776093c366bb883cc92f2e0b2bd7c517
th3-j4ck4l <maha678@icloud.com>
2019-10-17 08:53:51 +0300

    typo

--- discord-bot.py
+++ discord-bot.py
@@ -46,8 +46,7 @@ async def check_list(ctx):
 
 @bot.command()
 async def get_list(ctx):
-    await ctx.send(" ".join(list(map(lambda x: f'`{x}`', kill_list)))
-
+    await ctx.send(" ".join(list(map(lambda x: f'`{x}`', kill_list))))
 
 @bot.command()
 async def add_player(ctx, *args):
