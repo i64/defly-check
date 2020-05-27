0673835ba9c5968a6ab8a24caa2a875acb5242c5
1.v3m <16155914@mersin.edu.tr>
2020-05-27 05:47:21 +0300

    comment usued servers

--- worker.py
+++ worker.py
@@ -28,7 +28,15 @@ class GameModes(IntEnum):
 
 
 GEN_ENDPOINT = "https://s.defly.io/?r={}&m={}"
-REGION_LIST = ("EU1", "TOK1", "SA1", "RU1", "USE1", "USW1", "AU")
+REGION_LIST = (
+    "EU1",
+    # "TOK1",
+    # "SA1",
+    # "RU1",
+    "USE1",
+    "USW1",
+    # "AU",
+)
 KNOWN_PORTS = ("3005", "3015")
 
 trd_ss = None
